f0c8f9ada098525a092c77415588b2e52a69654a
Gray Sinclair <113727259+gsworkshell@users.noreply.github.com>
2022-11-28 11:45:34 -0600

    Add files via upload

--- Wordle Bot/Wordle main.py
+++ Wordle Bot/Wordle main.py
@@ -1,53 +1,71 @@
 # Wordle Project
 
 # holds list of 5 letter possible answers
-filter_list = []
+filt_list = []
 try:
-    with open('/Users/gray/PycharmProjects/pythonProject/Wordle_bot/5LA.txt', 'r') as answer_file:
+    with open('/Users/gray/PycharmProjects/pythonProject/Wordle_bot/Wordle Bot/5LA.txt', 'r') as answer_file:
         for line in answer_file:
-            filter_list.append(line.strip())
+            filt_list.append(line.strip())
 except FileNotFoundError:
     print('Unable to locate answer list.')
 
 print('\nMIT researchers have found the statistically optimal first guess is "SALET", which is a 15th century helm.')
-print('Possible Answers Left: ' + str(len(filter_list)))
+print('Possible Answers Left: ' + str(len(filt_list)))
+
 
 # input user interface
-# TODO make a function to change input word for multiple runs
-input_word = input("\nInput Word: ").lower()
-if 5 > len(input_word) > 5:
-    print('Wrong amount of letters. Please reset and try again.')
-else:
+
+
+def input_func():
+    input_word = input("\nInput Word: ").lower()
     print('\nColor Guide: g = green, y = yellow, b = black')
     print('Example: ggyyb\n')
-
-feedback = input('Feedback: ').lower()
-
-
-def filter_list_print():
-    print('Possible Answers Left: ' + str(len(filter_list)))
-    print(filter_list)
+    return input_word
 
 
-filt_list = tuple(filter_list)
+def feedback_func():
+    feedback = input('Feedback: ').lower()
+    return feedback
 
 
-def filter_func(new_list):
+def imput_wor_func():
+    input_wor = input_func()
+    return input_wor
+
+
+def feedback_wor_func():
+    feedback_wor = feedback_func()
+    return feedback_wor
+
+
+filter_list = tuple(filt_list)
+
+
+# TODO fix removal logic to prevent hyper-filtration
+def filter_func(new_list, input_wor, feedback):
     for item in new_list:
-        for i in range(5):  # TODO make input_word a variable or something to pass in
-            if feedback[i] == "b" and input_word[i] in item:
-                filter_list.remove(item)
-                break
-            elif feedback[i] == "g" and input_word[i] != item[i]:
-                filter_list.remove(item)
-                break
-            elif feedback[i] == "y" and input_word[i] not in item:
-                filter_list.remove(item)
-                break
-            elif feedback[i] == "y" and input_word[i] == item[i]:
-                filter_list.remove(item)
-                break
-    return filter_list
+        for i in range(len(item)):
+            if feedback[i] == "b" and input_wor[i] in item:
+                try:
+                    filt_list.remove(item)
+                except ValueError:
+                    pass
+            elif feedback[i] == "g" and input_wor[i] != item[i]:
+                try:
+                    filt_list.remove(item)
+                except ValueError:
+                    pass
+            elif feedback[i] == "y" and input_wor[i] not in item:
+                try:
+                    filt_list.remove(item)
+                except ValueError:
+                    pass
+            elif feedback[i] == "y" and input_wor[i] == item[i]:
+                try:
+                    filt_list.remove(item)
+                except ValueError:
+                    pass
+    return filt_list
 
 
 def print_ui(list1):
@@ -57,8 +75,6 @@ def print_ui(list1):
         print(word, end=', ')
 
 
-# TODO make this more pythonic
-run_1 = filter_func(filt_list)
-run_2 = filter_func(run_1)
-print_ui(run_1)
-print_ui(run_2)
+for num in range(6):
+    run_1 = filter_func(filter_list, imput_wor_func(), feedback_wor_func())
+    print_ui(run_1)
